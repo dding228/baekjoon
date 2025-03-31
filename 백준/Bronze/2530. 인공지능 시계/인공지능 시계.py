a96384a52f9960c6f2504ad9a20732268aca4c99
dding228 <101333069+dding228@users.noreply.github.com>
2025-03-31 19:42:53 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
@@ -1,8 +1,16 @@
-a,b,c = map(int,input().split())
-d = int(input())
+A, B, C = map(int, input().split())
+D = int(input())
 
-time = (a +(b+(c+d)//60)//60)% 24
-mi = (b+(c+d)//60) % 60
-sec= (c+d) % 60
+# 현재 시간 → 초로 변환
+current_time = A * 3600 + B * 60 + C
 
-print(time, mi, sec)
+# 요리 시간 더하기
+current_time += D
+
+# 시, 분, 초로 변환
+A = (current_time // 3600) % 24
+B = (current_time % 3600) // 60
+C = current_time % 60
+
+# 출력
+print(A, B, C)
